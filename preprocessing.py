eeccc4d4a848da47a4bf4c0677c43751f742eca0
Brooke <brookekelseyryan@gmail.com>
2021-04-30 14:05:09 -0700

    np array for everything except Teller file

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,11 @@ import os
 from pathlib import Path
 from os.path import dirname, abspath
 from shutil import copy2
+import numpy as np
+from PIL import Image
+import cv2
+
+from tensorflow.keras.preprocessing import image
 
 low_distortion_filenames = ["0-0-0",
                             "0-1-0",
@@ -31,14 +36,23 @@ low_distortion_filenames = ["0-0-0",
                             "4-1-90",
                             "4-1-135"]
 
+local_root = "/Users/brookeryan/Developer/Baldi Lab/Visual-Acuity/"
+remote_root = "/baldig/bioprojects2/VisualAcuity/"
+
 training_path = "/Users/brookeryan/Developer/Baldi Lab/Visual-Acuity/data/training/"
 testing_path = "/Users/brookeryan/Developer/Baldi Lab/Visual-Acuity/data/testing/"
 data_root = "/Users/brookeryan/Developer/Baldi Lab/Visual-Acuity/Multiple Choice Test 2/"
 
+x_train = np.zeros((2862, 400, 400, 3))
+x_test = np.zeros((13876, 400, 400, 3))
+
+y_train = np.zeros(2862, dtype=str)
+y_test = np.zeros(13876, dtype=str)
+
 
 def make_directories(dest_path):
     if not os.path.exists(dest_path):
-        # os.chmod(dest_path, 0o666)
+        os.chmod(dest_path, 0o666)
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
 
@@ -62,19 +76,39 @@ def is_icon(file):
     return file.count("-") < 2 and file.endswith(".png")
 
 
-def copy_to_testing_dir(rel_path, abs_path):
+def extract_label(path):
+    # we need the third element of the dashes
+    # angle = file_name.split("-")[-1]
+
+    # everythingelse/label/size/filename
+    return path.split("/")[-3]
+
+
+def copy_to_testing_dir(rel_path, abs_path, index):
     dest_path = os.path.join(testing_path, rel_path)
     make_directories(dest_path)
     copy2(abs_path, dest_path)
 
+    print(rel_path)
 
-def copy_to_training_dir(rel_path, abs_path):
+    x_test[index] = cv2.resize(cv2.imread(abs_path), (400, 400))
+
+    y_test[index] = extract_label(rel_path)
+
+
+def copy_to_training_dir(rel_path, abs_path, index):
     dest_path = os.path.join(training_path, rel_path)
     make_directories(dest_path)
     copy2(abs_path, dest_path)
 
+    x_train[index] = np.asarray(Image.open(abs_path))
+    y_train[index] = extract_label(rel_path)
+
 
 def sort():
+    num = 0
+    train = 0
+    test = 0
     for root, dirs, files in os.walk(data_root):
         for file in files:
             if file.endswith(".png") or file.endswith(".tif"):
@@ -84,17 +118,22 @@ def sort():
                 rel_path = abs_path.replace(data_root, "")
 
                 if is_low_distortion_file(file_name, rel_path):
-                    copy_to_training_dir(rel_path, abs_path)
+                    train += 1
+                    copy_to_training_dir(rel_path, abs_path, train)
 
                 elif is_icon(file):
                     pass
                     # copy_to_training_dir(rel_path, abs_path)
 
                 elif is_med_or_high_distortion_file(file_name, rel_path):
-                    copy_to_testing_dir(rel_path, abs_path)
+                    test += 1
+                    copy_to_testing_dir(rel_path, abs_path, test)
 
                 else:
                     print("Unable to determine what {x} is".format(x=file))
+    print(num)
+    print("train = {x}".format(x=train))
+    print("test={x}".format(x=test))
 
 
 # Main
